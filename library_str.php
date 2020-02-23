d814e4021f088f32529d4b60d8f2392555e25972
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 14:57:53 +0600

    first commit

---
+++ library_str.php
@@ -0,0 +1,11 @@
+<?php
+$str = "  abdus sattar bhuiyan   ";
+//$new_str = strtolower($str);
+//$new_str = strtoupper($str);
+//$new_str = ucfirst($str);
+ //$new_str =  ucwords($str);
+ // $new_str =  strlen($str);
+ $new_str =  trim($str);
+ echo "$str<br/>";
+echo $new_str;
+?>
